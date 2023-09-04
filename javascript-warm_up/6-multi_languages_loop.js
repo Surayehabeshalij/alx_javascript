724beff5fe8085a5454ab8de55fc2ff051440479
Surayehabeshalij <surafelshemsu@gmail.com>
2023-09-04 11:13:19 +0300

    here

--- javascript-warm_up/6-multi_languages_loop.js
+++ javascript-warm_up/6-multi_languages_loop.js
@@ -1,5 +1,4 @@
 #!/usr/bin/node
-
 const complements = ['C is fun', 'Python is cool', 'JavaScript is amazing'];
 for (const complement of complements) {
   console.log(complement);
--- javascript-warm_up/README.md
+++ javascript-warm_up/README.md
@@ -1,1 +1,4 @@
 here we see some quality javascript projects here in this course
+we see some changes here
+so we need to work with javascript
+
