e693c7cb2a55a7d2510d44c6fea9c663067b2e6f
Surayehabeshalij <137759541+Surayehabeshalij@users.noreply.github.com>
2023-09-11 14:02:47 +0300

    Update 100-starwars_characters.js

--- javascript-network/100-starwars_characters.js
+++ javascript-network/100-starwars_characters.js
@@ -1,10 +1,20 @@
 #!/usr/bin/node
-
 const request = require('request');
-const fs = require('fs');
-
-request(process.argv[2], function (err, response, body) {
-  if (err == null) {
-    fs.writeFileSync(process.argv[3], body);
+const url = 'https://swapi.co/api/films/' + process.argv[2];
+request(url, function (error, response, body) {
+  if (!error) {
+    let characters = JSON.parse(body).characters;
+    printCharacters(characters, 0);
   }
 });
+
+function printCharacters (characters, index) {
+  request(characters[index], function (error, response, body) {
+    if (!error) {
+      console.log(JSON.parse(body).name);
+      if (index + 1 < characters.length) {
+        printCharacters(characters, index + 1);
+      }
+    }
+  });
+}
