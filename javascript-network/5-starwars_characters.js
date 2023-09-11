47071477bd9ad23ce7bd82ef4a76da53457876d8
Surayehabeshalij <137759541+Surayehabeshalij@users.noreply.github.com>
2023-09-11 14:00:10 +0300

    Update 5-starwars_characters.js

--- javascript-network/5-starwars_characters.js
+++ javascript-network/5-starwars_characters.js
@@ -1,24 +1,10 @@
 #!/usr/bin/node
+
 const request = require('request');
+const fs = require('fs');
 
-function printMovieCharacters(movieId) {
-  const url = `https://swapi.dev/api/films/${movieId}/`;
-
-  request(url, { json: true }, (error, response, body) => {
-    if (response.statusCode === 200) {
-      const characters = body.characters;
-      characters.forEach((characterUrl) => {
-        request(characterUrl, { json: true }, (error, response, body) => {
-          if (response.statusCode === 200) {
-            const characterName = body.name;
-            console.log(characterName);
-          } else {
-            console.log(`Failed to fetch character data for URL: ${characterUrl}`);
-          }
-        });
-      });
-    } else {
-      console.log(`Failed to fetch movie data for ID: ${movieId}`);
-    }
-  });
-}
+request(process.argv[2], function (err, response, body) {
+  if (err == null) {
+    fs.writeFileSync(process.argv[3], body);
+  }
+});
