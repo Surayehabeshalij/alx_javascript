05e5a0b544498a7a5ca4a77991500f3bb69c24de
Surayehabeshalij <surafelshemsu@gmail.com>
2023-09-11 13:59:39 +0300

    final

--- javascript-network/100-starwars_characters.js
+++ javascript-network/100-starwars_characters.js
@@ -1,21 +1,10 @@
 #!/usr/bin/node
 
-const req = require('request');
-const id = process.argv[2];
-const url = 'https://swapi-api.hbtn.io/api/films/';
-req.get(url + id, function (error, res, body) {
-  if (error) {
-    console.log(error);
-  }
-  const data = JSON.parse(body);
-  const dd = data.characters;
-  for (const i of dd) {
-    req.get(i, function (error, res, body1) {
-      if (error) {
-        console.log(error);
-      }
-      const data1 = JSON.parse(body1);
-      console.log(data1.name);
-    });
+const request = require('request');
+const fs = require('fs');
+
+request(process.argv[2], function (err, response, body) {
+  if (err == null) {
+    fs.writeFileSync(process.argv[3], body);
   }
 });
