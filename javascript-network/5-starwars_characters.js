1a932031a43f33a1ab52d72b0257a09108e54de6
Surayehabeshalij <surafelshemsu@gmail.com>
2023-09-11 10:06:36 +0300

    final

--- javascript-network/5-starwars_characters.js
+++ javascript-network/5-starwars_characters.js
@@ -1,5 +1,6 @@
 #!/usr/bin/node
 const request = require('request');
+
 const movieId = process.argv[2];
 const baseUrl = 'https://swapi-api.alx-tools.com/api/films/';
 const fullUrl = baseUrl.concat(movieId);
