c780bf94285f912c20ebf2706e8604b2fbce72b0
Surayehabeshalij <surafelshemsu@gmail.com>
2023-09-12 09:16:15 -0700

    final

---
+++ Javascript_advanced/0-welcome.js
@@ -0,0 +1,10 @@
+#!/usr/bin/node
+//Lexical scoping and welcome message
+const welcome = (firstName, lastName) => {
+    const fullName = `${firstName} ${lastName}`;
+
+    const displayFullName = () => {
+        alert(`Welcome ${fullName}!`)
+    }
+    displayFullName()
+}
---
+++ Javascript_advanced/README.md
@@ -0,0 +1,1 @@
+here is more to understand how the java script works on the internet
