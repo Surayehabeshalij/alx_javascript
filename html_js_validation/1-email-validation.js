126657aef639629bd110f3d8d2a9a8d35dcb4a25
Surayehabeshalij <surafelshemsu@gmail.com>
2023-09-27 11:24:19 -0700

    for

--- html_js_validation/1-email-validation.js
+++ html_js_validation/1-email-validation.js
@@ -1,21 +1,27 @@
+// Get references to the form and error message elements
+const emailForm = document.getElementById("emailForm");
+const emailInput = document.getElementById("email");
+const errorElement = document.getElementById("error");
+
+// Function to validate the email format
 function validateEmail(event) {
-    event.preventDefault(); // Prevent the default form submission behavior
-  
-    var email = document.getElementById('email').value;
-  
-    // Define the regular expression for email format validation
-    var emailRegex = /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
-  
-    // Check if the email follows the standard email format
-    if (!emailRegex.test(email)) {
-      document.getElementById('error').textContent = 'Please enter a valid email address.';
-      return false;
-    }
-  
-    // If the email format is correct, allow form submission
-    return true;
+  // Get the entered email value
+  const email = emailInput.value;
+
+  // Regular expression to check email format
+  const emailRegex = /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,4}$/;
+
+  // Check if the email format is correct
+  if (!emailRegex.test(email)) {
+    // Prevent form submission
+    event.preventDefault();
+    // Display error message
+    errorElement.textContent = "Please enter a valid email address.";
+  } else {
+    // Clear any previous error messages
+    errorElement.textContent = "";
   }
-  
-  // Get the form element and attach the event listener for form submission
-  var form = document.getElementById('emailForm');
-  form.addEventListener('submit', validateEmail);
+}
+
+// Add a submit event listener to the form
+emailForm.addEventListener("submit", validateEmail);
